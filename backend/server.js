5b6f093165878ea4bd9b143a28befe4f64aedfda
Abizer <abizergadi5253@gmail.com>
2025-08-20 16:50:22 +0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -15,21 +15,13 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true //to allow cookies to be sent to frontend
 }))
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth/", authRoutes)
 app.use("/api/users/", userRoutes)
 app.use("/api/chat", chatRoutes)
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-
-}
 
 app.listen(PORT, ()=> {
     console.log("server is running on port", PORT);
