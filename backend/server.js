7a0e2f82a2abc0196f2fa75e7a1dd10fece63e22
Abizer <abizergadi5253@gmail.com>
2025-08-20 17:06:56 +0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chattr-app-1gs2.vercel.app/",
     credentials: true //to allow cookies to be sent to frontend
 }))
 
